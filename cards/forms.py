b3c4e5332f8528b40d7e5ed250418c265230206f
burjui <burjui.dota@mail.ru>
2024-04-02 21:23:02 +0300

    Работаю с Джанго

--- cards/forms.py
+++ cards/forms.py
@@ -1,3 +1,6 @@
 from django import forms
-from .models import Card
 
+
+class CardForm(forms.Form):
+    question = forms.CharField(label='Вопрос', max_length=100)
+    answer = forms.CharField(label='Ответ', widget=forms.Textarea(attrs={'rows': 10, 'cols': 30}))
--- cards/models.py
+++ cards/models.py
@@ -1,93 +1,91 @@
 from django.db import models
+from django.urls import reverse
+import logging
 
-# Create your models here.
-class Card(models.Model):
-     card_id = models.AutoField(primary_key=True, db_column="CardID")
-     question = models.TextField(db_column="Question")
-     answer = models.TextField(db_column="Answer")
-     category_id = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True, db_column="CategoryID")
-     upload_date = models.DateTimeField(auto_now_add=True, db_column="UploadDate")
-     views = models.IntegerField(default=0, db_column="Views")
-     favorites = models.IntegerField(default=0, db_column="Favorites")
-     tags = models.ManyToManyField('Tag', related_name='cards', through="CardTags")
+# Создаем или получаем экземпляр логгера
+logger = logging.getLogger(__name__)
 
-     # Расширение нашего класса
-     class Meta:
-         db_table = 'Cards'
-         verbose_name = 'Карточка'
-         verbose_name_plural = 'Карточки'
+# Устанавливаем базовый уровень логирования. В продакшене вы можете выбрать уровень WARNING или ERROR
+logging.basicConfig(level=logging.DEBUG)
 
-     def __str__(self):
-         return self.question
-
-     def get_absolute_url(self):
-         return reverse('detail_card_by_id', kwargs={'card_id': self.card_id})
-
-# Тестируем связь многие ко многим (ManyToManyField), создаем для примера класс Tag
 
 class Category(models.Model):
-    category_id = models.AutoField(primary_key=True, db_column="CategoryID")
-    name = models.CharField(max_length=100, unique=True, db_column="Name")
+    category_id = models.AutoField(primary_key=True, db_column='CategoryID')
+    name = models.CharField(max_length=255, unique=True, db_column='Name')
 
     class Meta:
-        db_table = "Categories"
-        verbose_name = "Категория"
-        verbose_name_plural = "Категории"
+        db_table = 'Categories'
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
 
     def __str__(self):
         return self.name
 
-class User(models.Model):
-    user_id = models.AutoField(primary_key=True, db_column="UserID")
-    user_name = models.TextField(max_length=100, db_column="UserName")
-
-    class Meta:
-        db_table = "Users"
-        verbose_name = "Пользователь"
-        verbose_name_plural = "Пользователи"
-
-    def __str__(self):
-        return self.user_name
 
 class Tag(models.Model):
-    tag_id = models.AutoField(primary_key=True, db_column="TagID")
-    name = models.CharField(max_length=50, unique=True, db_column="Name")
+    tag_id = models.AutoField(primary_key=True, db_column='TagID')
+    name = models.CharField(max_length=255, unique=True, db_column='Name')
 
     class Meta:
-        db_table = "Tags"
+        db_table = 'Tags'
         verbose_name = 'Тег'
         verbose_name_plural = 'Теги'
 
     def __str__(self):
         return self.name
-"""
-Добавляем теги
-tag1 = Tag.objects.create(name="Python")
-tag2 = Tag.objects.create(name="Django")
-tag3 = Tag.objects.create(name="Flask")
-Сохраняем теги
-Создаем карточки с тегами
-card = Card.objects.create(question="Как создать карточку?", answer="Использовать метод Create()")
-card.tags.add(tag1, tag2)
-Добываем последнюю карточку
-last_card = Card.objects.last()
-Получаем все теги
-tag_all = card.tags.all()
-"""
+
+
+class Card(models.Model):
+    class Status(models.IntegerChoices):
+        UNCHECKED = 0, 'Не проверено'
+        CHECKED = 1, 'Проверено'
+
+    card_id = models.AutoField(primary_key=True, db_column='CardID')
+    question = models.TextField(db_column='Question')
+    answer = models.TextField(db_column='Answer')
+    category_id = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, db_column='CategoryID', verbose_name='Категория')
+    upload_date = models.DateTimeField(auto_now_add=True, db_column='UploadDate', verbose_name='Дата загрузки')
+    views = models.IntegerField(default=0, db_column='Views')
+    favorites = models.IntegerField(default=0, db_column='Favorites')
+    # Через map bool мы будем приводить 0 и 1 к False и True
+    check_status = models.BooleanField(choices=tuple(map(lambda x: (bool(x[0]), x[1]), Status.choices)),
+                                       default=Status.UNCHECKED, db_column='CheckStatus')
+    # Непосредственное определение связи многие ко многим с моделью Tag
+    tags = models.ManyToManyField('Tag', related_name='cards', through='CardTags')
+
+    # through - это модель, которая будет использоваться для связи многие ко многим
+
+    class Meta:
+        db_table = 'Cards'
+        verbose_name = 'Карточка'
+        verbose_name_plural = 'Карточки'
+
+    def __str__(self):
+        return self.question
+
+    # Опишем get_absolute_url для модели Card - метод, который возвращает URL карточки
+    # Псевдоним - detail_card_by_id
+    # reverse - возвращает URL по псевдониму
+    def get_absolute_url(self):
+        return reverse('detail_card_by_id', kwargs={'card_id': self.card_id})
+
+    def save(self, *args, **kwargs):
+        # Логируем перед сохранением объекта
+        logger.debug(f'Сохранение карточки {self.card_id}, значения: {self.__dict__}')
+
+        super().save(*args, **kwargs)  # Вызываем оригинальный метод save
+
 
 class CardTags(models.Model):
-    id = models.AutoField(primary_key=True, db_column='ID')
+    id = models.AutoField(primary_key=True, db_column='id')
     card = models.ForeignKey('Card', on_delete=models.CASCADE, db_column='CardID')
     tag = models.ForeignKey('Tag', on_delete=models.CASCADE, db_column='TagID')
 
     class Meta:
         db_table = 'CardTags'
+        unique_together = (('card', 'tag'),)
         verbose_name = 'Связь карточка-тег'
         verbose_name_plural = 'Связи карточка-тег'
 
     def __str__(self):
         return f'{self.card} - {self.tag}'
-
-
-
-
